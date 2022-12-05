6009615e85e8d8b93b9d85b7508c3169cbd02fd5
Diogo-Coding <diogo.hcp.2001@gmail.com>
2022-12-05 22:39:14 +0000

    Otros cambios

--- sign.php
+++ sign.php
@@ -5,15 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/header.css">
-	<link rel="stylesheet" href="css/footer.css">
 	<link rel="stylesheet" href="css/font-awesome.min.css">
     <title>Sign up page</title>
 </head>
 <body>
     <?php include('includes/header.php') ?>
     <main>
-        <hr>
+        <br>
     </main>
     <?php include('includes/footer.php') ?>
 </body>
