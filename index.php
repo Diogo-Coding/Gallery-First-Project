964e9912026ac5704a794a0874c5e84f9b8c0897
Diogo-Coding <diogo.hcp.2001@gmail.com>
2022-12-05 23:38:03 +0000

    mover todo

--- index.php
+++ index.php
@@ -4,11 +4,10 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="js/bootstrap.bundle.min.js">
+	<link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
 	<link rel="stylesheet" href="css/index.css">
 	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<script src="js/bootstrap.bundle.min.js"></script>
+	<script src="js/bootstrap/bootstrap.bundle.min.js"></script>
 	<script src="js/jquery-3.3.1.slim.min.js"></script>
 	<title>Galeria Adrian y Diogo</title>
 </head>
