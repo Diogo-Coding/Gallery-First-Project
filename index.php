d76fd425584ba35b194de588ab1e2d3d4d8c7374
Diogo-Coding <diogo.hcp.2001@gmail.com>
2022-12-05 23:21:34 +0000

    no se

--- index.php
+++ index.php
@@ -6,8 +6,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="js/bootstrap.bundle.min.js">
-	<link rel="stylesheet" href="css/header.css">
-	<link rel="stylesheet" href="css/footer.css">
 	<link rel="stylesheet" href="css/index.css">
 	<link rel="stylesheet" href="css/font-awesome.min.css">
 	<script src="js/bootstrap.bundle.min.js"></script>
