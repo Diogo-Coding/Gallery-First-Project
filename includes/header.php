062f09b7fe22f10aa6c461ec4dedcaf17837a781
Diogo-Coding <diogo.hcp.2001@gmail.com>
2022-12-06 01:24:39 +0000

    a

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,4 @@
-<!-- Nav -->
+<!-- Header nav -->
 <header class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
   <div class="container">
     <a class="navbar-brand" href="index.php">Galeria</a>
