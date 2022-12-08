3543716a7fa48da516b0677f8d26068dff5626cd
Diogo-Coding <diogo.hcp.2001@gmail.com>
2022-12-08 18:02:54 +0000

    Redireccionamientos correctos

--- pages/includes/log-error.php
+++ pages/includes/log-error.php
@@ -1,3 +1,3 @@
 <main>
-    <div class="alert alert-error">¡Error at sign up! <a href="../../index.php">Try again</a></div>
+    <div class="alert alert-error">¡Error at sign up! <a href="/pages/sign.php">Try again</a></div>
 </main>
--- pages/includes/log-incorrect-login.php
+++ pages/includes/log-incorrect-login.php
@@ -1,3 +1,3 @@
 <main>
-    <div class="alert alert-error">¡Incorrect login, email or password are incorrect! <a href="../../index.php">Try again</a></div>
+    <div class="alert alert-error">¡Incorrect login, email or password are incorrect! <a href="/pages/sign.php">Try again</a></div>
 </main>
